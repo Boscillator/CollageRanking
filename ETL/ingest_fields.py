d2bfb12ad31aabde09c5924c053e3d3a01fc3b9a
Fred Buchanan <oscillator.b@gmail.com>
2020-03-09 10:44:25 -0400

    Commented ingest_fields.py

--- ETL/ingest_fields.py
+++ ETL/ingest_fields.py
@@ -13,10 +13,13 @@ import argparse
 
 def get_type(row):
     if row['API data type'] == 'string':
+        # I don't want to handle strings yet
         return 'skip'
     elif row['API data type'] == 'integer' and pd.notna(row['LABEL']):
+        # Integer types with labels should be interpreted like enums, not integers
         return 'factor'
     else:
+        # The variable contains a number
         return 'quantity'
 
 def main(dictionary_file):
@@ -25,6 +28,7 @@ def main(dictionary_file):
     results = []
     for idx, row in df.iterrows():
         if pd.notna(row['NAME OF DATA ELEMENT']):
+            # Rows with a name are a variable, create it based on it's type
             type = get_type(row)
             if type == 'skip':
                 continue
@@ -36,7 +40,7 @@ def main(dictionary_file):
                     category = row['dev-category'],
                     developer_name = row['developer-friendly name'],
                     source = row['SOURCE'],
-                    consumer = row['NOTES'] == 'Shown/used on consumer website.'
+                    consumer = row['NOTES'] == 'Shown/used on consumer website.'    # Variables with this note are probably important
                 ))
             elif type == 'factor':
                 results.append(FactorVariable(
@@ -53,6 +57,7 @@ def main(dictionary_file):
             else:
                 raise ValueError(f'Got unexpected type {type}')
         else:
+            # The row does not specify an name, it must reference additional factors for the previous row
             results[-1].factors[str(int(row['VALUE']))] = row['LABEL']
 
     print("inserting")
@@ -61,8 +66,8 @@ def main(dictionary_file):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dictionary', type=argparse.FileType('rb'))
-    parser.add_argument('--mongo', type=str, default='CollegeRank')
+    parser.add_argument('dictionary', type=argparse.FileType('rb'), help='Path to a the CollageScorecardDataDictionary. Expected to be a `xlsx` file.')
+    parser.add_argument('--mongo', type=str, default='CollegeRank', help="Mongoengine connection string")
     args = parser.parse_args()
 
     connect(args.mongo)    
